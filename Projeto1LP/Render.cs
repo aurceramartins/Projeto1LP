2b364ef3515fa6de6f4f00394970dc9f3264a933
Pedro Santos <pedrosantosobral@gmail.com>
2018-04-18 11:28:01 +0100

    add render class

---
+++ Projeto1LP/Render.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto1LP
+{
+    class Render
+    {
+        char[,] symbols = new char[7, 7];
+        public void RenderBoard(Board board)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    symbols[i, j] = PiecesName(board.GetBoard(new Position(i, j)));
+
+                    Console.Write($" {symbols[i, j]} ");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("---------------------");
+        }
+        private char PiecesName(Pieces pieces)
+        {
+            switch (pieces)
+            {
+                case Pieces.RedCircle: return 'r';
+                case Pieces.RedCube: return 'R';
+                case Pieces.WhiteCircle: return 'w';
+                case Pieces.WhiteCube: return 'W';
+                case Pieces.None:return '|';
+                default: return ' ';
+            }
+        }
+
+    }
+}

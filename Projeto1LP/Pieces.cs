7ad04333704071598b9f50d1915bd96834b17017
Joana Marques <joana.pipaa@gmail.com>
2018-04-18 11:40:05 +0100

    Make some changes: class Pieces

--- Projeto1LP/Pieces.cs
+++ Projeto1LP/Pieces.cs
@@ -4,11 +4,11 @@ namespace Projeto1LP
 
     public enum Pieces
     {
-        R, // Red cube
-        r, // Red circle
-        W, // White cube
-        w, // White circle
-        |  //None
-    };
+        None,
+        RedCube,
+        RedCircle,
+        WhiteCube,
+        WhiteCircle
+    }
 
 }

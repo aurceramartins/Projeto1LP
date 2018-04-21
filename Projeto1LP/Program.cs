f32f15c60007ef7ef44f243214777822e7e1c11d
Pedro Santos <pedrosantosobral@gmail.com>
2018-04-21 16:45:29 +0100

    Add comments

--- Projeto1LP/Program.cs
+++ Projeto1LP/Program.cs
@@ -2,10 +2,18 @@
 
 namespace Projeto1LP
 {
+    /// <summary>
+    /// Program.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
         static void Main(string[] args)
         {
+            ///initializes the game
             Controller play = new Controller();
             play.Play();
         }

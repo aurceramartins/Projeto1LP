57453f2a2550ca8537e7b3fe66bce28bc41ee61e
Pedro Santos <pedrosantosobral@gmail.com>
2018-04-21 16:57:10 +0100

    Add comment

--- Projeto1LP/Render.cs
+++ Projeto1LP/Render.cs
@@ -4,38 +4,64 @@ using System.Text;
 
 namespace Projeto1LP
 {
+    /// <summary>
+    /// Render.
+    /// </summary>
     class Render
     {
+        /// <summary>
+        /// The symbols array.
+        /// </summary>
         char[,] symbols = new char[7, 7];
+        /// <summary>
+        /// Renders the board.
+        /// </summary>
+        /// <param name="board">Board.</param>
         public void RenderBoard(Board board)
         {
+            ///clean the command line
             Console.Clear();
-
+            ///Go through the lines array
             for (int i = 0; i < 7; i++)
             {
+                ///Go through the columns array
                 for (int j = 0; j < 7; j++)
                 {
+                    ///Saves the board positions in the symbols array
                     symbols[i, j] = PiecesName(board.GetBoard(new Position(i, j)));
-
+                    ///prints the board
                     Console.Write($" {symbols[i, j]} ");
                 }
                 Console.Write("\n");
             }
+            ///prints the lower board part 
             Console.WriteLine("---------------------");
             Console.WriteLine(" 1  2  3  4  5  6  7 \n");
         }
+        /// <summary>
+        /// Diferent pieces.
+        /// </summary>
+        /// <returns>The name.</returns>
+        /// <param name="pieces">Pieces.</param>
         private char PiecesName(Pieces pieces)
         {
+            ///print the choosed pieces
             switch (pieces)
             {
                 case Pieces.RedCircle: return 'r';
                 case Pieces.RedCube: return 'R';
                 case Pieces.WhiteCircle: return 'w';
                 case Pieces.WhiteCube: return 'W';
+                ///No piece
                 case Pieces.None: return '|';
                 default: return ' ';
             }
         }
+        /// <summary>
+        /// Result the specified winner.
+        /// </summary>
+        /// <returns>The result.</returns>
+        /// <param name="win">Window.</param>
         public void Result(int win)
         {
             switch (win)
@@ -45,6 +71,11 @@ namespace Projeto1LP
                 default: Console.WriteLine("Its a Draw!!"); break;
             }
         }
+        /// <summary>
+        /// prints the player turn
+        /// </summary>
+        /// <returns>The nplayer.</returns>
+        /// <param name="player">Player.</param>
         public void Nplayer(int player)
         {
             switch (player)

69eb7260109712c00d758ee865a6576835ce621b
Pedro Santos <pedrosantosobral@gmail.com>
2018-04-21 15:46:25 +0100

    Add comments

--- Projeto1LP/Check4win.cs
+++ Projeto1LP/Check4win.cs
@@ -6,19 +6,35 @@ namespace Projeto1LP
 {
     class Check4Win
     {
+        /// <summary>
+        /// Returns the winner
+        /// </summary>
+        /// <returns>int Player</returns>
+        /// <param name="board">Board.</param>
+        /// <param name="nplayer">Nplayer.</param>
         public int Check(Board board, int nplayer)
         {
             if (CheckWinner(board, nplayer)) return nplayer;
             else return 0;
         }
+        /// <summary>
+        /// Checks the winner.
+        /// </summary>
+        /// <returns><c>true</c>, if winner was checked, <c>false</c> otherwise.</returns>
+        /// <param name="board">Board.</param>
+        /// <param name="nplayer">Nplayer.</param>
         public bool CheckWinner(Board board, int nplayer)
         {
+            ///go through the rows arrays.
             for (int i = 6; i >= 0; i--)
             {
+                ///go through the columns array.
                 for (int j = 6; j >= 0; j--)
                 {
+                    ///Verefy all the possible vertical positions
                     if (j >= 3)
                     {
+                        ///calls the CheckBoard method and give 4 positions.
                         if (CheckBoard(board, new Position[] {
                         new Position(i, j),
                         new Position(i, j-1),
@@ -28,8 +44,10 @@ namespace Projeto1LP
                             return true;
                         }
                     }
+                    ///Verefy all the possible horizontal positions
                     else if (i >= 3)
                     {
+                        ///calls the CheckBoard method and give 4 positions.
                         if (CheckBoard(board, new Position[] {
                         new Position(i, j),
                         new Position(i-1, j),
@@ -39,8 +57,10 @@ namespace Projeto1LP
                             return true;
                         }
                     }
+                    ///Verefy all the possible left diagonal positions
                     if (i >= 3 && j >= 3)
                     {
+                        ///calls the CheckBoard method and give 4 positions.
                         if (CheckBoard(board, new Position[] {
                         new Position(i, j),
                         new Position(i-1, j-1),
@@ -50,8 +70,10 @@ namespace Projeto1LP
                             return true;
                         }
                     }
+                    ///Verefy all the possible right diagonal positions
                     if (i >= 3 && j <= 3)
                     {
+                        ///calls the CheckBoard method and give 4 positions.
                         if (CheckBoard(board, new Position[] {
                         new Position(i, j),
                         new Position(i-1, j+1),
@@ -65,82 +87,115 @@ namespace Projeto1LP
             }
             return false;
         }
-
+        /// <summary>
+        /// Checks the diferent win conditions.
+        /// </summary>
+        /// <returns><c>true</c>, if board was checked, <c>false</c> otherwise.</returns>
+        /// <param name="board">Board.</param>
+        /// <param name="positions">Positions.</param>
+        /// <param name="nplayer">Nplayer.</param>
         private bool CheckBoard(Board board, Position[] positions, int nplayer)
         {
+            ///counters
             int white = 0;
             int cube = 0;
             int circle = 0;
             int red = 0;
 
+            ///go through the 4 given positions
             foreach (Position position in positions)
             {
+
+                ///checks for same color pieces(red pieces)
                 if (board.GetBoard(position) == Pieces.RedCircle
                     || board.GetBoard(position) == Pieces.RedCube)
                 {
+                    ///if threre are 4 red pieces
                     if (red == 3)
                     {
+                        //checks if its player 1 
                         if (nplayer == 1)
                         {
+                            ///Set the red counter to 0
                             red = 0;
                             return true;
                         }
                     }
+                    ///Increases the red counter in 1 value
                     red++;
                 }
                 else
                 {
+                    ///set the red counter to 0
                     red = 0;
                 }
+                ///checks for same shape pieces(cubes)
                 if (board.GetBoard(position) == Pieces.RedCube
                     || board.GetBoard(position) == Pieces.WhiteCube)
                 {
+                    ///if threre are 4 red pieces
                     if (cube == 3)
                     {
+                        ///checks if its player 1 
                         if (nplayer == 1)
                         {
+                            ///Set the cube counter to 0
                             cube = 0;
                             return true;
                         }
                     }
+                    ///Increases the cube counter in 1 value
                     cube++;
                 }
                 else
                 {
+                    ///set the counter to 0
                     cube = 0;
                 }
+                ///checks for same shape pieces(circle)
                 if (board.GetBoard(position) == Pieces.WhiteCircle
                     || board.GetBoard(position) == Pieces.RedCircle)
                 {
-                    if (nplayer == 2)
+                    ///if threre are 4 circle pieces
+                    if (circle == 3)
                     {
-                        if (circle == 3)
+                        ///checks if its player 2
+                        if (nplayer == 2)
                         {
+                            ///Set the circle counter to 0
                             circle = 0;
                             return true;
                         }
                     }
+                    ///Increases the cicle counter in 1 value
                     circle++;
                 }
                 else
                 {
+                    ///set the counter to 0
                     circle = 0;
                 }
+                ///checks for same color pieces(white pieces)
                 if (board.GetBoard(position) == Pieces.WhiteCube
                     || board.GetBoard(position) == Pieces.WhiteCircle)
                 {
+                    ///if threre are 4 white pieces
                     if (white == 3)
                     {
+                        //checks if its player 2 
                         if (nplayer == 2)
                         {
+                            ///Set the white counter to 0
                             white = 0;
                             return true;
                         }
                     }
+                    ///Increases the white counter in 1 value
                     white++;
                 }
                 else
                 {
+                    ///Set the white counter to 0
                     white = 0;
                 }
             }
